f9009f4e912122b466412b48d8d4047f52c20fa3
savoo1 <savo436@gmail.com>
2021-08-18 17:34:06 +0200

    change

--- js/main.js
+++ js/main.js
@@ -200,49 +200,9 @@ $( ".h_sec3 .item .forimgs_video .playbtn" ).click(function(e) {
 });
 
 
-
-document.onreadystatechange = function(e)
-{
-  if(document.readyState=="interactive")
-  {
-    var all = document.getElementsByTagName("*");
-    for (var i=0, max=all.length; i < max; i++) 
-    {
-      set_ele(all[i]);
-    }
-  }
-}
-
-function check_element(ele)
-{
-  var all = document.getElementsByTagName("*");
-  var totalele=all.length;
-  var per_inc=100/all.length;
-
-  if($(ele).on())
-  {
-    var prog_width=per_inc+Number(document.getElementById("progress_width").value);
-    document.getElementById("progress_width").value=prog_width;
-    $("#bar1").animate({width:prog_width+"%"},10,function(){
-      if(document.getElementById("bar1").style.width=="100%")
-      {
-        // $(".progress").fadeOut("slow");
-      }     
-    });
-  }
-
-  else  
-  {
-    set_ele(ele);
-  }
-}
 $( window ).on( "load", function() {
     $(".progress").addClass("progress_done");
 });
-function set_ele(set_element)
-{
-  check_element(set_element);
-}
 
 
 
